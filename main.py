1c2f0e35b6330b5d472c2612a418375b542d2ed9
gianlucamazzillo <gianlucamazzillo@hotmail.com>
2025-04-13 21:55:20 -0300

    Added propellant atributes

--- main.py
+++ main.py
@@ -10,4 +10,26 @@ full_cea_output = cea_obj.get_full_cea_output(Pc=[1000], # number or list of cha
                                 pc_units='psia', # pc_units = 'psia', 'bar', 'atm', 'mmh'
                                 output='siunits',# output = 'calories' or 'siunits'
                                 fac_CR=None)     # finite area combustor, contraction ratio,)
-print(full_cea_output)
+
+Tcomb = cea_obj.get_Tcomb(Pc=1000,
+                          MR=5.0
+                          ) #Tcomb = temperatura de combustão
+print(f'Tcomb: {Tcomb/1.8:.2f}°C') #.2f sempre vai deixar em duas casas decimais
+
+Densities = cea_obj.get_Densities(Pc=1000,
+                                   MR =5.0,
+                                   eps=8.0)
+print(f'Densities: {Densities}')
+
+ch_molwt_g, ch_gamma = cea_obj.get_Chamber_MolWt_gamma(
+            Pc=1000, MR=5.0, eps=8.0
+        )
+print (f'Massa Molar: {ch_molwt_g:.3f}\n'
+       f'Gamma:{ch_gamma:.3f}')
+
+ex_molwt_g, ex_gamma = cea_obj.get_exit_MolWt_gamma(
+            Pc=1000, MR=5.0, eps=8.0
+        )
+print (f'Massa Molar de saída: {ex_molwt_g:.3f}\n'
+       f'Gamma de saída:{ex_gamma:.3f}')
+                            
